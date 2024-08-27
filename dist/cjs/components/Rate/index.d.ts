98b75427ca2b8215b19301a8da559e8ed475508b
davidchoy98 <38069452+davidchoy98@users.noreply.github.com>
2024-08-27 17:07:09 +0800

    Build Assets

--- dist/cjs/components/Rate/index.d.ts
+++ dist/cjs/components/Rate/index.d.ts
@@ -1,4 +1,4 @@
 import { ReactElement } from "react";
 import { RateProps } from "./Rate.types";
 export declare const Rate: ({ rate, rateOptions, onRateChange }: RateProps) => ReactElement<RateProps>;
-//# sourceMappingURL=Rate.d.ts.map
+//# sourceMappingURL=index.d.ts.map
